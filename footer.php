1b8fce73b6a33bd591def49a96879fac7fa9e15c
Georgie Luhur <hello@georgie.nu>
2014-03-06 22:46:29 +1100

    Basic header and footer section build

---
+++ footer.php
@@ -0,0 +1,6 @@
+		</div>
+		<footer class="site-footer" role="contentinfo">
+			&copy; Hey Georgie, 2007-<?php $year = date("Y"); echo $year; ?> &middot; designed by <a href="http://georgieluhur.com">Georgie</a> &middot; &uarr; <a href="#page-top">top</a>
+		</footer>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+	<head>
+		<title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+		<meta charset="utf-8">
+		<meta name="description" content="The tales of a web designer, photographer, musician, writer – someone who loves everything from A to Z.">
+	</head>
+	<body>
+		<header class="site-header">
+			<h1 class="site-title">Hey Georgie</h1>
+			<nav class="main-navigation" role="navigation">
+				<!-- This is an example link -->
+				<a class="main-navigation-link" href="/">Home</a>
+			</nav>
+		</header>
+		<div class="site-content">
