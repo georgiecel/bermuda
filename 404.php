a29f25a855dff65d642284b0fe4410bd6e62fb3d
Georgie Luhur <hello@georgie.nu>
2014-03-08 21:41:49 +1100

    Adding 404.php for error template

---
+++ 404.php
@@ -0,0 +1,21 @@
+		<?php get_header(); ?>
+			<div class="post">
+				<h1>This page has just blown up.</h1>
+				<img class="image-alignright alignright" src="<?php bloginfo('template_url'); ?>/images/creeper.png">
+				<p>Shit. <a href="/contact/">Please tell me about this</a>. I might have forgotten to install a door somewhere.</p>
+				<h2>Frequently asked questions about missing pages</h2>
+				<h3>I’m looking for photos?</h3>
+				<p>Starting in 2014, I moved some of my photoblog posts from the domain Indecisively.me, to here. I have tried my best to redirect all links from there to their corresponding posts here, however, I may have missed a few. Or it may be the case that the post you are after has been removed. <a href="/contact/">Want to check with me?</a></p>
+				<h3>I’m looking for a tutorial that used to be here?</h3>
+				<p>I may have deleted it. Chances are, you will find a similar tutorial to the one you were looking for, elsewhere on the internet. But if you really, really need to see my version, I might have a backup of it. <a href="/contact/">Feeling lucky?</a></p>
+				<h3>What the hell happened to your free WordPress themes?</h3>
+				<p>They were simply not up to standard, and I decided to remove them. Not up to standard means that they were not responsive/mobile friendly, the HTML was not so well formatted, the CSS could have been minified, and the themes overall could be optimised. At the time I created them, they were quite well done, but in the year 2013, nope.</p>
+				<p>Still want to use a shitty theme? I might have a copy I can send you, if you’re <a href="/contact/">still feeling lucky</a>.</p>
+				<h3>Are you going to make more free WordPress themes?</h3>
+				<p>No. I’m a busy girl. <em>*cries*</em> Frankly I don’t have time to make free things for people because it takes up a lot of my time and is honestly not a priority right now.</p>
+				<p>However, I do paid custom designs if you would like to work with me. Are you <a href="/contact/">feeling super lucky even though I will not make a theme for free for anyone (so don’t even ask)?</a></p>
+				<h3>I came here from somewhere and the link was <em>obviously</em> wrong.</h3>
+				<p>Well, do good for the world, why don’t you, and <a href="/contact/">drop me a line</a> telling me the page you came from and where the link was.</p>
+			<?php get_search_form(); ?>
+			</div>
+		<?php get_footer(); ?>
