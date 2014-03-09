2dda7fdeb7279f12b48936421d7c75ebd287979b
Georgie Luhur <hello@georgie.nu>
2014-03-09 20:03:17 +1100

    Adding comments.php for comment template; inserting important comment functions

---
+++ comments.php
@@ -0,0 +1,55 @@
+		<?php // Do not delete these lines
+			if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
+				die ('Please do not load this page directly. Thanks!');
+			if ( post_password_required() ) {
+				return;
+			}
+		?>
+			<section class="comments">
+		<?php if ( have_comments() ) : ?>
+			<h1 class="comments-title post-title">Comments</h1>
+			<section class="comments-list">
+				<?php wp_list_comments(array('walker' => new comment_walker() ));
+				// 'style=section&callback=better_comment&end-callback=better_comment_close');
+				?>
+			</section>
+		<?php endif; ?>
+		<?php
+    		if ( ! comments_open() ) : // There are comments but comments are now closed
+				echo '<p class="comments-closed">Comments are closed.</p>';
+    		endif; 
+		?>
+		<?php if ('open' == $post->comment_status) : ?>
+			<div class="comments-respond">
+				<h2 class="comments-respond-title"><?php comment_form_title( 'Leave a Comment', 'Reply to %s' ); ?></h2>
+				<form class="comments-respond-form" action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" role="form">
+				<?php if ( $user_ID ) : ?>
+					<p class="comments-respond-form-row">Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="Log out of this account">Log out &raquo;</a></p>
+				<?php else : ?>
+					<p class="comments-respond-form-row">
+						<input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
+						<label for="author"><small>Name <?php if ($req) echo "(required)"; ?></small></label>
+					</p>
+					<p class="comments-respond-form-row">
+						<input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
+						<label for="email"><small>Email (will not be published) <?php if ($req) echo "(required)"; ?></small></label>
+					</p>
+					<p class="comments-respond-form-row">
+						<input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" tabindex="3" />
+						<label for="url"><small>Website</small></label>
+					</p>
+				<?php endif; ?>
+					<?php if ( function_exists(cs_print_smilies) ) {cs_print_smilies();} ?>
+					<p class="comments-respond-form-row">
+						<textarea name="comment" id="comment" cols="30" rows="10" tabindex="4"></textarea>
+					</p>
+					<p class="comments-respond-form-row">
+						<input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment">
+					</p>
+					<?php comment_id_fields( $post_id ); ?>
+					<?php do_action( 'comment_form', $post_id ); ?>
+				</form>
+				<p class="comments-cancel-reply"><small><?php cancel_comment_reply_link() ?></small></p>
+			</div>
+		</section>
+	<?php endif; ?>
--- functions.php
+++ functions.php
@@ -36,4 +36,119 @@
 		return sizeof(explode(" ", $postcontent));
 	}
 
+	// awesome semantic comment
+
+	function better_comment($comment, $args, $depth) {
+		$GLOBALS['comment'] = $comment;
+		extract($args, EXTR_SKIP);
+		
+		if ( 'article' == $args['style'] ) {
+			$tag = 'article';
+			$add_below = 'comment';
+		} else {
+			$tag = 'article';
+			$add_below = 'comment';
+		}
+
+		?>
+		<<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' :'parent') ?> id="comment-<?php comment_ID() ?>" itemscope itemtype="http://schema.org/Comment">
+			<figure class="gravatar"><?php echo get_avatar( $comment, 65, 'http://hey.georgie.nu/wp-content/themes/heygeorgie/images/bg.png', 'Author’s gravatar' ); ?></figure>
+			<div class="comment-meta post-meta" role="complementary">
+				<h2 class="comment-author">
+					<a class="comment-author-link" href="<?php comment_author_url(); ?>" itemprop="author"><?php comment_author(); ?></a>
+				</h2>
+				<time class="comment-meta-item" datetime="<?php comment_date('Y-m-d') ?>T<?php comment_time('H:iP') ?>" itemprop="datePublished"><?php comment_date('jS F Y') ?>, <a href="#comment-<?php comment_ID() ?>" itemprop="url"><?php comment_time() ?></a></time>
+				<?php edit_comment_link('<p class="comment-meta-item">Edit this comment</p>','',''); ?>
+				<?php if ($comment->comment_approved == '0') : ?>
+				<p class="comment-meta-item">Your comment is awaiting moderation.</p>
+				<?php endif; ?>
+			</div>
+			<div class="comment-content post-content" itemprop="text">
+				<?php comment_text() ?>
+				<div class="comment-reply">
+					<?php comment_reply_link(array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+				</div>
+			</div>
+		<?php }
+
+	// end of awesome semantic comment
+
+	function better_comment_close() {
+		echo '</article>';
+	}
+
+	class comment_walker extends Walker_Comment {
+		var $tree_type = 'comment';
+		var $db_fields = array( 'parent' => 'comment_parent', 'id' => 'comment_ID' );
+ 
+		/** CONSTRUCTOR
+		* You'll have to use this if you plan to get to the top of the comments list, as
+		* start_lvl() only goes as high as 1 deep nested comments */
+		function __construct() { ?>
+
+			<section class="comments">
+
+		<?php }
+
+		/** START_LVL
+	     * Starts the list before the CHILD elements are added. */
+		function start_lvl( &$output, $depth = 0, $args = array() ) {
+			$GLOBALS['comment_depth'] = $depth + 2; ?>
+			
+			<section class="child-comments comments-list">
+
+		<?php }
+	
+		/** END_LVL
+		 * Ends the children list of after the elements are added. */
+		function end_lvl( &$output, $depth = 0, $args = array() ) {
+			$GLOBALS['comment_depth'] = $depth + 2; ?>
+
+			</section><!-- /.children -->
+ 
+		<?php }
+
+		/** START_EL */
+		function start_el( &$output, $comment, $depth, $args, $id = 0 ) {
+			$depth++;
+			$GLOBALS['comment_depth'] = $depth;
+			$GLOBALS['comment'] = $comment;
+			$parent_class = ( empty( $args['has_children'] ) ? '' : 'parent' ); ?>
+
+			<article <?php comment_class(empty( $args['has_children'] ) ? '' :'parent') ?> id="comment-<?php comment_ID() ?>" itemscope itemtype="http://schema.org/Comment">
+				<figure class="gravatar"><?php echo get_avatar( $comment, 65, 'http://hey.georgie.nu/wp-content/themes/heygeorgie/images/bg.png', 'Author’s gravatar' ); ?></figure>
+				<div class="comment-meta post-meta" role="complementary">
+					<h2 class="comment-author">
+						<a class="comment-author-link" href="<?php comment_author_url(); ?>" itemprop="author"><?php comment_author(); ?></a>
+					</h2>
+					<time class="comment-meta-item" datetime="<?php comment_date('Y-m-d') ?>T<?php comment_time('H:iP') ?>" itemprop="datePublished"><?php comment_date('jS F Y') ?>, <a href="#comment-<?php comment_ID() ?>" itemprop="url"><?php comment_time() ?></a></time>
+					<?php edit_comment_link('<p class="comment-meta-item">Edit this comment</p>','',''); ?>
+					<?php if ($comment->comment_approved == '0') : ?>
+					<p class="comment-meta-item">Your comment is awaiting moderation.</p>
+					<?php endif; ?>
+				</div>
+				<div class="comment-content post-content" itemprop="text">
+					<?php comment_text() ?>
+					<div class="comment-reply">
+						<?php comment_reply_link(array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+					</div>
+				</div>			
+
+		<?php }		
+
+		function end_el(&$output, $comment, $depth = 0, $args = array() ) { ?>
+
+			</article><!-- /#comment-' . get_comment_ID() . ' -->
+
+		<?php }
+
+		/** DESTRUCTOR */
+		function __destruct() { ?>
+
+			</section><!-- /#comment-list -->
+		
+		<?php }
+
+	}
+
 ?>
