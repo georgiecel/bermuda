191291b358cacf33c4bf92abf3c3d717b20ad982
Georgie Luhur <hello@georgie.nu>
2014-03-12 14:06:15 +1100

    Adding post thumbnail to individual post

--- single.php
+++ single.php
@@ -1,7 +1,12 @@
 		<?php get_header(); ?>
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<article class="post" itemscope itemtype="http://schema.org/BlogPosting" role="article">
-			<h1 class="post-title" itemprop="name"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>" class="title" itemprop="url" role="heading"><?php the_title(); ?></a></h1>
+			<?php if ( has_post_thumbnail() ) { ?>
+				<div class="post-thumbnail-wrapper">
+					<figure class="post-thumbnail"><?php the_post_thumbnail(); ?></figure>
+				</div>
+			<?php } ?>
+				<h1 class="post-title" itemprop="name"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>" class="title" itemprop="url" role="heading"><?php the_title(); ?></a></h1>
 				<footer class="post-meta" role="complementary">
 					<meta itemprop="author" content="Georgie Luhur">
 					<meta itemprop="inLanguage" content="en">
