d5a142c7a363384fdf5efa7ae542add29117b50f
Georgie Luhur <hello@georgie.nu>
2014-03-18 23:31:49 +1100

    Editing sidebar and applying colours

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,11 @@
 		<aside class="site-sidebar">
 			<!-- Insert logo here -->
 			<div class="site-sidebar-widget">
-				
+				<h3 class="site-sidebar-heading">Hello!</h3>
+				<p class="site-sidebar-text">I’m Georgie, a front-end web developer, concert photographer &amp; tea obsessed pescetarian</p>
+			</div>
+			<div class="site-sidebar-widget">
+				<h3 class="site-sidebar-heading">The LZRGUN Manifesto</h3>
+				<p class="site-sidebar-text">Everyone is a friend unless proven otherwise. Don’t write lists; don’t plan; let priorities grow in your mind. Have something menial in every week. Simplify. Let go, but never hate. Appreciate. Optimism is a road that you create. Embrace the morning. Believe in magic. Love is a heavy word.</p>
 			</div>
 		</aside>
