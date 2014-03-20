df02240439df97bc8962d62303d10c8cca7f3b50
Georgie Luhur <hello@georgie.nu>
2014-03-20 14:51:58 +1100

    Minor updates to typography and colours

--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,6 @@
 			</div>
 			<div class="site-sidebar-widget">
 				<h3 class="site-sidebar-heading">The LZRGUN Manifesto</h3>
-				<p class="site-sidebar-text">Everyone is a friend unless proven otherwise. Don’t write lists; don’t plan; let priorities grow in your mind. Have something menial in every week. Simplify. Let go, but never hate. Appreciate. Optimism is a road that you create. Embrace the morning. Believe in magic. Love is a heavy word.</p>
+				<p class="site-sidebar-text">Everyone is a friend unless proven otherwise. Simplify your life. Appreciate. Optimism is a road that you create. Embrace the morning. Believe in magic. Love is a heavy word.</p>
 			</div>
 		</aside>
