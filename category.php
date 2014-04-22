37c9ef1843394e1a0ff5728edafd910a134ae40c
Georgie Luhur <hello@georgie.nu>
2014-04-22 22:49:26 +1000

    Update to category descriptions

--- category.php
+++ category.php
@@ -8,18 +8,63 @@
 				<?php elseif (is_category('2 minutes and 40 seconds')) : ?>
 					<p><strong>2 minutes and 40 seconds</strong> is an interview segment in which I interview people from any walk of life, and ask them questions outside of the traditional interview format to reveal random facts, suss out their quirks, and provide an enjoyable read. To find out more about this segment, <a href="/2-minutes-40-seconds/">read this page</a>.</p>
 					<p>A new ‘2 minutes and 40 seconds’ is posted every second Thursday.</p>
+				<?php elseif (is_category('A Day in the Life')) : ?>
+					<p><strong>A Day in the Life</strong> is part of <a href="http://breakthesky.net/tag/day-life/">Manda’s ‘A Day in the Life’</a> monthly linkup. In this linkup, bloggers document their daily life once a month.</p>
+					<p>I was really excited when Manda started this linkup because I used to do a photography ‘A Day in the Life’ on my photoblog, which was organised by Tracey of sh1ft.org/chasethelight.org. </p>
+				<?php elseif (is_category('Brandon')) : ?>
+					<p>Posts in this category mention my younger brother <strong>Brandon</strong> who recently started a Bachelor of Arts in Pschology. We used to fight and argue a lot when we were younger, but got along better as we grew up.</p>
+					<p>We have a lot of funny conversations, and although we have varied interests, we like to share thoughts about music, social events, restaurants, and technology. He calls me Cici (chie-chie) about 99% of the time &mdash; “Cici” means “big sister” in Indonesian &mdash; the other 1% of the time is some variant like Ci, Chip, or Chips.</p>
+				<?php elseif (is_category('Daniel')) : ?>
+					<p><strong>Daniel</strong> is a friend of mine who resides in Singapore. He is a moderator on <a href="http://stackoverflow.com">Stack Overflow</a> and is often known as BoltClock online. He’s a good listener, a great writer, and an even better friend!</p>
+					<p>Thanks to Daniel I now have a secret dream of being a member of Australian children’s musical group Hi-5, but don’t tell anyone that.</p>
 				<?php elseif (is_category('Fashion Friday')) : ?>
 					<p><strong>Fashion Friday</strong> is an fashion segment I started in 2013, where I share outfits or items related to fashion and beauty.</p>
 					<p>There used to be a new post each month, on average, but now occurs every second Friday. Some older posts have been posted on Saturday rather than Friday, due to timezones, but are now more consistent.</p>
 					<p>I would like to thank one of my best friends <a href="http://alonelyseptember.org">Seb</a> for having the time and patience to photograph in some of the more recent posts.</p>
+				<?php elseif (is_category('Health')) : ?>
+					<p><strong>Health</strong> discusses everything health, though mostly related to my personal life. I have had a countless amount of health issues related to my digestive system and mental illness.</p>
+					<p>In particular, I have had two main issues. Firstly, I had a twelve-year battle with high cholesterol despite being extremely thin. I was constantly told that genetics did not contribute to my condition. I tried every diet on the planet to reduce my cholesterol levels, but to no avail.  Visiting a new doctor in 2013 led me to discover that my condition was indeed genetic, and I was put on medication.</p>
+					<p>My second issue is related to depression, which I suffered from since 2004. I went through bouts of being suicidal, and self-harmed a lot of the time. I visited a counsellor in 2008, and began taking medication in 2011. I stopped in 2013, feeling a lot better.</p>
+					<p>Occasionally, I still get anxious, and there are times I have been extremely stressed and on edge. The last time I recall feeling suicidal was in late 2013, whereupon I called a friend to talk to. It was the first time in about a year that I had had thoughts of suicide.</p>
+				<?php elseif (is_category('Hong Kong')) : ?>
+					<p>I visited <strong>Hong Kong</strong> for the second time, in December 2013. I first visited in about 1996 when I was six years old, and apparently lost my first tooth there. I really liked the McDonald’s Happy Meal toys.</p>
+					<p>My boyfriend James and I went there for Christmas for about a week. Posts in this category are about our travels.</p>
+				<?php elseif (is_category('James')) : ?>
+					<p>I properly met <strong>James</strong> in 2004 when he had a fake map and pretended he knew where me and my friend Jennifer were going. We soon discovered his trick.</p>
+					<p>In 2007, we became very good friends. James helped me through a rough patch with another friend, and became my shoulder to cry on. We started a relationship in early 2008 and are still together. He’s been there for the good times and the bad, and has been there for me when I have needed it.</p>
+				<?php elseif (is_category('Japan')) : ?>
+					<p>I adore <strong>Japan</strong>. I studied the language in high school for about four years. I don’t remember very much of the vocabulary, but I have very good reading ability of hiragana and katakana characters. Kanji not so much, though.</p>
+					<p>I absolutely adore Japanese cuisine and culture. The cuisine really suits me because I am a pescetarian and don’t eat land meat or red meat; I do, however, eat fish, shellfish and white sea meat.</p>
+					<p>My boyfriend James and I visited Japan in December 2013 when it was winter. Posts in this category are about our travels.</p>
+				<?php elseif (is_category('Lilian')) : ?>
+					<p><strong>Lilian</strong> is my best (and pretty much only) girl friend, and we’ve known each other for nearly twelve years. We used to have a lot in common but our interests have drifted apart. But &mdash; we are still the best of friends. I trust her with everything.</p>
+				<?php elseif (is_category('Music')) : ?>
+					<p>The <strong>Music</strong> category is one of my biggest categories. Posts in this category are often reviews of concerts I have been to, general discussion about bands I like, my experience playing piano or other instruments, or photos of concerts. Photos of concerts are likely to be in the <a href="/category/photoblog/">Photoblog</a> category as well.</p>
 				<?php elseif (is_category('Photoblog')) : ?>
 					<p>The <strong>Photoblog</strong> category is currently “in-progress”. It contains posts that contain mainly photographs, or where the photographs are the focus of the post.</p>
 					<p>I had a photoblog for about four years, initially at www.o1s.net from January 2009 to February 2010, which was later located at <a href="http://indecisively.me">Indecisively.me</a>. In late 2013 I made the decision to move the posts from my photoblog here to my blog, because I had been sharing photos at <em>Hey Georgie</em> more often than on my photoblog.</p>
 					<p>As at April 2014, I am still gradually moving the posts and removing old and unnecessary posts.</p>
-				<?php elseif (is_category('Music')) : ?>
-					<p>The <strong>Music</strong> category is one of my biggest categories. Posts in this category are often reviews of concerts I have been to, general discussion about bands I like, my experience playing piano or other instruments, or photos of concerts. Photos of concerts are likely to be in the <a href="/category/photoblog/">Photoblog</a> category as well.</p>
+				<?php elseif (is_category('School')) : ?>
+					<p>I started blogging long before the posts in this category might suggest. In fact, I was in my last year of high school when I started blogging more actively and independently. <strong>School</strong> is unlikely to contain posts that I wrote while I was actually still in school; my oldest post dates to 2009 which is when I was already in university.</p>
+					<p>Posts in this category may refer to incidents that happened while I was in school, or may tell stories from that period of time.</p>
+				<?php elseif (is_category('Tristan')) : ?>
+					<p>Also known as Toffeecake, Honeydew or Tris, <strong>Tristan</strong> is my sweetest, dearest friend who also happens to live down the road from me. Not technically neighbours, but the definition works. We met in 2013, and I don’t exaggerate when I say he helped me find myself. &hearts; He’s pretty much my best friend.</p>
+					<p>Tristan and I are completely ridiculous in each other’s company &mdash; we’ll go to gigs together and dance; we’ll sing; he’ll probably try to tickle me occasionally; we’ll call each other names (both nasty and cheesy). But honestly, I think I would be lost without him.</p>
+				<?php elseif (is_category('University')) : ?>
+					<p>I studied a Bachelor of Arts in Communication (Information and Media) from 2009 to 2011. I also studied a Master of Interactive Multimedia from 2012 to 2013. The latter was extremely challenging as I had been working full-time and also attended classes four nights a week after work.</p>
+					<p>I enjoyed a lot of what I did in my Bachelors degree, especially the research work. I also completed minors in film photography and 2D animation. I completed three theses in my media submajor, focusing on the following:</p>
+					<ul>
+						<li>Young adults’ expression of self in internet spaces</li>
+						<li>How people with personal blogs engage in reflective practice</li>
+						<li>Twitter’s impact on users’ imagined audience and trust in social networking</li>
+					</ul>
+					<p>My major (group) project in my Masters degree was focused on creating an online network to support carers of sufferers of Alzheimer’s Disease.</p>
 				<?php else : ?>
-					<p>You are currently viewing the <strong><?php single_cat_title(); ?></strong> category.</p>
+					<p>You are currently viewing the <strong><?php single_cat_title(); ?></strong> category. This category doesn’t have a fancy custom description yet, probably because:</p>
+					<ul>
+						<li>It doesn’t really need one because it’s self-explanatory.</li>
+						<li>I haven’t gotten around to it yet because it’s new.</li>
+					</ul>
 				<?php endif; ?>
 			</div>
 			<!-- // begin the loop -->
@@ -44,7 +89,8 @@
 						</a></p>
 					</footer>
 					<div class="post-content" itemprop="text">
-						<?php the_content('','[...]'); ?>
+						<?php the_excerpt('','[...]'); ?>
+						<p><a class="btn more-link" href="<?php the_permalink() ?>">Read more of this post »</a></p>
 					</div>
 				</article>
 			<?php endwhile; ?>
