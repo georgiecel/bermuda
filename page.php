14bce0d34fc3e66642df58f7432c921f6ddc79c3
Georgie Luhur <hello@georgie.nu>
2015-07-16 23:34:59 +1000

    Minor adjustments to margins

--- page.php
+++ page.php
@@ -7,12 +7,10 @@
 						echo '<h1 class="post-title" itemprop="name" role="heading">', the_title(), '</h1>';
 					}
 				?>
-				<footer class="post-meta" role="contentinfo">
-					<meta itemprop="author" content="<?php the_author(); ?>">
-					<meta itemprop="inLanguage" content="en">
-					<meta itemprop="url" content="<?php echo get_permalink(); ?>">
-					<meta itemprop="wordCount" content="<?php echo wordcount(); ?>">
-				</footer>
+				<meta itemprop="author" content="<?php the_author(); ?>">
+				<meta itemprop="inLanguage" content="en">
+				<meta itemprop="url" content="<?php echo get_permalink(); ?>">
+				<meta itemprop="wordCount" content="<?php echo wordcount(); ?>">
 				<div class="post-content" itemprop="articleBody">
 					<?php the_content(); ?>
 					<?php edit_post_link('Click here to edit this page (admin).'); ?>
