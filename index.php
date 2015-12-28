6be85da5220f492aaa93afa83d1965131798e805
Georgie Luhur <hello@georgie.nu>
2015-12-28 11:30:40 +1100

    Cleaning up post metadata

--- index.php
+++ index.php
@@ -6,21 +6,13 @@
 					<figure class="post-thumbnail"><?php the_post_thumbnail(); ?></figure>
 				<?php } ?>
 					<h1 class="post-title" itemprop="name" role="heading"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>" class="title" itemprop="url"><?php the_title(); ?></a></h1>
-					<footer class="post-meta" role="contentinfo">
-						<time class="post-meta-item" datetime="<?php the_time('Y-m-d'); ?>T<?php the_time('H:iP'); ?>" itemprop="datePublished"><?php the_time('jS F Y'); ?></time>
-						<div class="post-meta-item">Posted in <span itemprop="keywords"><?php the_category(', '); ?></span></div>
-						<div class="post-meta-item">
-							<a href="<?php comments_link(); ?>" itemprop="discussionUrl">
-								<span itemprop="interactionCount"><?php comments_number( '0 comments', '1 comment', '% comments' ); ?></span>
-							</a>
-						</div>
-						<meta itemprop="author" content="<?php the_author(); ?>">
-						<meta itemprop="inLanguage" content="en">
-						<meta itemprop="copyrightYear" content="<?php the_time('Y'); ?>">
-						<meta itemprop="commentCount" content="<?php echo get_comments_number(); ?>">
-						<meta itemprop="thumbnailUrl" content="<?php $image_id = get_post_thumbnail_id(); $image_url = wp_get_attachment_image_src($image_id,'large', true); echo $image_url[0]; ?>">
-						<?php image_url_meta(); ?>
-					</footer>
+					<time class="post-meta-item" datetime="<?php the_time('Y-m-d'); ?>T<?php the_time('H:iP'); ?>" itemprop="datePublished"><?php the_time('jS F Y'); ?></time>
+					<meta itemprop="author" content="<?php the_author(); ?>">
+					<meta itemprop="inLanguage" content="en">
+					<meta itemprop="copyrightYear" content="<?php the_time('Y'); ?>">
+					<meta itemprop="commentCount" content="<?php echo get_comments_number(); ?>">
+					<meta itemprop="thumbnailUrl" content="<?php $image_id = get_post_thumbnail_id(); $image_url = wp_get_attachment_image_src($image_id,'large', true); echo $image_url[0]; ?>">
+					<?php image_url_meta(); ?>
 					<div class="post-content" itemprop="text">
 						<?php the_excerpt(); ?>
 					</div>
--- single.php
+++ single.php
@@ -4,7 +4,8 @@
 			<?php if ( has_post_thumbnail() ) { ?>
 				<figure class="post-thumbnail"><?php the_post_thumbnail(); ?></figure>
 			<?php } ?>
-				<h1 class="post-title" itemprop="name" role="heading"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>" class="title" itemprop="url"><?php the_title(); ?></a></h1>
+			<?php $html_title = get_post_meta($post->ID, 'html_title', true); ?>
+				<h1 class="post-title" itemprop="name" role="heading"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>" class="title" itemprop="url"><?php if ($html_title) { echo $html_title; } else { the_title(); } ?></a></h1>
 				<footer class="post-meta" role="contentinfo">
 					<time class="post-meta-item" datetime="<?php the_time('Y-m-d'); ?>T<?php the_time('H:iP'); ?>" itemprop="datePublished"><?php the_time('jS F Y'); ?></time>
 					<div class="post-meta-item">Posted in <span itemprop="keywords"><?php the_category(', '); ?></span></div>
