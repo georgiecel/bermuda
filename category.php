5f3581eac5f76a8bac9205b8b77b31691c07bb64
Georgie Luhur <hello@georgie.nu>
2015-12-30 23:17:12 +1100

    Removing post thumbnail from category view

--- category.php
+++ category.php
@@ -14,9 +14,6 @@
 				</div>
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<article class="post" itemscope itemtype="http://schema.org/BlogPosting" role="article">
-				<?php if ( has_post_thumbnail() ) { ?>
-					<figure class="post-thumbnail"><?php the_post_thumbnail(); ?></figure>
-				<?php } ?>
 					<h1 class="post-title" itemprop="name"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>" class="title" itemprop="url" role="heading"><?php the_title(); ?></a></h1>
 					<footer class="post-meta" role="complementary">
 						<time class="post-meta-item" datetime="<?php the_time('Y-m-d'); ?>T<?php the_time('H:iP'); ?>" itemprop="datePublished"><?php the_time('jS F Y'); ?></time>
