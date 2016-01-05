a4e3e6cdce4d17a10f6c5c8fb02a65ec5b3d78fd
Georgie Luhur <hello@georgie.nu>
2016-01-05 23:08:55 +1100

    Updating individual post title

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 		<?php get_header(); ?>
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<article class="post" itemscope itemtype="http://schema.org/BlogPosting" role="article">
-				<h1 class="post-title" itemprop="name" role="heading"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>" class="title" itemprop="url"><?php if ($html_title) { echo $html_title; } else { the_title(); } ?></a></h1>
+				<h1 class="post__title" itemprop="name" role="heading"><?php if ($html_title) { echo $html_title; } else { the_title(); } ?></h1>
 				<footer class="post-meta" role="contentinfo">
 					<div class="post-meta-item"><time datetime="<?php the_time('Y-m-d'); ?>T<?php the_time('H:iP'); ?>" itemprop="datePublished"><?php the_time('jS F Y'); ?></time></div>
 					<div class="post-meta-item">Posted in <span itemprop="keywords"><?php the_category(', '); ?></span></div>
@@ -21,7 +21,6 @@
 				</footer>
 				<div class="post-content" itemprop="text">
 					<?php the_content(); ?>
-					<?php edit_post_link('Click here to edit this entry (admin).'); ?>
 				</div>
 			</article>
 			<nav class="pagination">
