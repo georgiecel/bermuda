57b730cf1808ed1bdc0a51c379db4e191ec985be
Georgie Luhur <hello@georgie.nu>
2016-02-03 23:39:42 +1100

    Removing unneeded JavaScript

--- footer.php
+++ footer.php
@@ -6,7 +6,6 @@
 			</footer>
 		</div>
 		<?php wp_footer(); ?>
-		<script src="<?php bloginfo('template_url'); ?>/js/script.min.js"></script>
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
