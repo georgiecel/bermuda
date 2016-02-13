cbbad9765a3b8e96d92dc467eb4978530aceffb6
Georgie Luhur <hello@georgie.nu>
2016-02-13 15:26:45 +1100

    Updating error page

--- 404.php
+++ 404.php
@@ -1,18 +1,16 @@
-		<?php get_header(); ?>
-			<div class="post error-post">
-				<div class="post-404"></div>
-				<h1 role="heading" class="post-title error-title">The bermuda triangle doesn’t exist.</h1>
-				<h2>And neither does this page.</h2>
-				<p>If you think the page you were after <em>should</em> actually exist, <a href="/contact/">please notify me</a>, because I may have genuinely made a mistake in a link or deleted a page.</p>
-				<h3>Frequently asked questions about missing pages</h3>
-				<h4>I’m looking for a tutorial that used to be here?</h4>
-				<p>I will no longer post tutorials to my blog. A lot of my tutorials have been moved to <a href="http://tuts.by.georgie.nu/">tuts.by.georgie.nu</a> (I know, smart URL, huh?). If you don’t find what you want there, I may have deleted it. Chances are, you will find a similar tutorial to the one you were looking for, elsewhere on the internet.</p>
-				<h4>I’m looking for photos?</h4>
-				<p>In 2014, I moved some of my photoblog posts from the domain Indecisively.me, to here. I have tried my best to redirect all links to their corresponding posts here, however, I may have missed a few. Or it may be the case that the post you are after has been removed. <a href="/contact/">Want to check with me?</a></p>
-				<h4>What happened to your free WordPress themes?</h4>
-				<p>They were simply not up to standard, and I decided to remove them. They were not responsive/mobile friendly, the HTML was not so well formatted, the CSS could have been minified, and the themes overall could be optimised. At the time I created them, they were quite well done, but in the year 2013, nope. But I was kind enough to save the PHP and WordPress versions <a href="https://copy.com/SwhKb1MI9o3Y">on Copy.com</a>, so have at it, and you’re welcome.</p>
-				<h4>Are you going to make more free WordPress themes?</h4>
-				<p>No. I’m a busy girl. <em>*cries*</em> Frankly I don’t have time to make free things for people because it takes up a lot of my time and is honestly not a priority right now.</p>
-				<p>However, I do paid custom designs if you would like to work with me. <a href="http://georgieluhur.com">Check out my portfolio.</a></p>
-			</div>
-		<?php get_footer(); ?>
+<!doctype html>
+<html <?php language_attributes(); ?> class="site-error">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="initial-scale=1.0, width=device-width">
+		<title>Oops.</title>
+		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.min.css?<?php echo date('Ymd', filemtime( get_stylesheet_directory() . '/style.min.css' )); ?>" type="text/css" media="screen">
+	</head>
+	<body class="site-error__inner">
+		<div class="site-error__content">
+			<h1 class="site-error__title">The bermuda triangle doesn’t exist&hellip;</h1>
+			<p>And neither does this page. 😢</p>
+			<p><script>document.write('<a class="site-error__link" href="' + document.referrer + '">Go back</a>');</script>, or <a class="site-error__link" href="mailto:404@georgie.nu">let me know</a>.</p>
+		</div>
+	</body>
+</html>
