eabe913091a37f7be4b8cb191e868a44d547b84c
Georgie Luhur <hello@georgie.nu>
2016-03-02 21:03:38 +1100

    Updating archive templates

--- archive.php
+++ archive.php
@@ -1,41 +1,48 @@
-<?php 
-	// Template Name: Archives
-	get_header();
-	if ( have_posts() ) : while ( have_posts() ) : the_post();
-?>
-	<article class="post page" itemscope itemtype="http://schema.org/Article" role="article">
-		<h1 class="post__title" itemprop="name" role="heading">Post Archive</h1>
-		<meta itemprop="author" content="<?php the_author(); ?>">
-		<meta itemprop="inLanguage" content="en">
-		<meta itemprop="url" content="<?php echo get_permalink(); ?>">
-		<meta itemprop="wordCount" content="<?php echo wordcount(); ?>">
-		<div class="post__content" itemprop="articleBody">
-			<?php the_content(); ?>
-			<h2>Last ten posts</h2>
-			<ul>
-				<?php wp_get_archives('type=postbypost&limit=10'); ?>
-			</ul>
-			<h2>Authors</h2>
-			<ul>
-				<?php wp_list_authors('optioncount=1'); ?>
-			</ul>
-			<h2>Archives by year</h2>
-			<ul>
-				<?php wp_get_archives('type=yearly'); ?>
-			</ul>
-			<h2>Archives by month</h2>
-			<ul>
-				<?php wp_get_archives('type=monthly'); ?>
-			</ul>
-			<h2>Categories</h2>
-			<ul>
-				<?php wp_list_categories('title_li=&hierarchical=1'); ?>
-			</ul>
+<?php get_header(); ?>
+	<section class="homepage-listing post-listing">
+<?php if ( have_posts() ) : ?>
+		<div class="post-listing__intro">
+			<h1 class="post__title">Archives for <?php echo get_the_archive_title(); ?></h1>
+			<?php while ( have_posts() ) : the_post(); ?>
 		</div>
-	</article>
-<?php 
-	endwhile;
-	else :
-	endif;
-	get_footer(); 
-?>
+		<article class="post" itemscope itemtype="http://schema.org/BlogPosting">
+			<?php $html_title = get_post_meta($post->ID, 'html_title', true); ?>
+			<h2 class="post-summary__title" itemprop="name"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>" itemprop="url"><?php if ($html_title) { echo $html_title; } else { the_title(); } ?></a></h2>
+			<div class="post__meta-item">
+				<time datetime="<?php the_time('Y-m-d'); ?>T<?php the_time('H:iP'); ?>" itemprop="datePublished"><?php the_time('jS F Y'); ?></time>
+			</div>
+			<meta itemprop="author" content="<?php the_author(); ?>">
+			<meta itemprop="inLanguage" content="en">
+			<meta itemprop="copyrightYear" content="<?php the_time('Y'); ?>">
+			<meta itemprop="commentCount" content="<?php echo get_comments_number(); ?>">
+			<meta itemprop="thumbnailUrl" content="<?php $image_id = get_post_thumbnail_id(); $image_url = wp_get_attachment_image_src($image_id,'large', true); echo $image_url[0]; ?>">
+			<?php image_url_meta(); ?>
+			<div class="post-summary__content" itemprop="text">
+				<?php the_excerpt(); ?>
+			</div>
+		</article>
+	<?php endwhile; ?>
+	</section>
+	<nav class="pagination pagination--home">
+		<?php previous_posts_link('<div class="pagination__item-container pagination__item-container--left">
+				<div class="pagination__item">
+					<span class="pagination__arrow" role="presentation" aria-hidden="true">&laquo;</span>
+					<div class="pagination__link-container">
+						<span class="pagination__link-text">Newer posts</span>
+					</div>
+				</div>
+			</div>');
+		?>
+		<?php next_posts_link('<div class="pagination__item-container pagination__item-container--right">
+				<div class="pagination__item">
+					<span class="pagination__arrow" role="presentation" aria-hidden="true">&raquo;</span>
+					<div class="pagination__link-container">
+						<span class="pagination__link-text">Older posts</span>
+					</div>
+				</div>
+			</div>');
+		?>
+	</nav>
+	<?php else : ?>
+<?php endif; ?>
+<?php get_footer(); ?>
---
+++ page-archives.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+	<article class="post page" itemscope itemtype="http://schema.org/Article">
+		<h1 class="post__title" itemprop="name">Post Archive</h1>
+		<meta itemprop="author" content="<?php the_author(); ?>">
+		<meta itemprop="inLanguage" content="en">
+		<meta itemprop="url" content="<?php echo get_permalink(); ?>">
+		<meta itemprop="wordCount" content="<?php echo wordcount(); ?>">
+		<div class="post__content" itemprop="articleBody">
+			<?php the_content(); ?>
+			<h2>Last ten posts</h2>
+			<ul>
+				<?php wp_get_archives('type=postbypost&limit=10'); ?>
+			</ul>
+			<h2>Authors</h2>
+			<ul>
+				<?php wp_list_authors('optioncount=1'); ?>
+			</ul>
+			<h2>Archives by year</h2>
+			<ul>
+				<?php wp_get_archives('type=yearly'); ?>
+			</ul>
+			<h2>Archives by month</h2>
+			<ul>
+				<?php wp_get_archives('type=monthly'); ?>
+			</ul>
+			<h2>Categories</h2>
+			<ul>
+				<?php wp_list_categories('title_li=&hierarchical=1'); ?>
+			</ul>
+		</div>
+	</article>
+<?php 
+	if ( have_posts() ) : while ( have_posts() ) : the_post();
+	endwhile;
+	else :
+	endif;
+	get_footer(); 
+?>
